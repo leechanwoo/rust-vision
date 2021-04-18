cde5cb8a0f9b3ffbeeb762d4d6c1d1228a45a939
leechanwoo <cjswosa2@gmail.com>
2021-04-18 22:41:27 +0900

    build

--- actix-test/docker_run.sh
+++ actix-test/docker_run.sh
@@ -5,5 +5,5 @@ docker run \
     -p $1:$1 \
     -v $(pwd):/workspace \
     -w /workspace \
-    rust-vision \
+    cjswosa22/rust-vision:0.1 \
     bash
--- actix-test/src/main.rs
+++ actix-test/src/main.rs
@@ -17,54 +17,34 @@ use futures::{future::ok, stream::once};
 
 use std::{thread, time};
 
-struct MyWs;
+struct MonitorWs;
 
-impl Actor for MyWs {
+impl Actor for MonitorWs {
     type Context = ws::WebsocketContext<Self>;
 }
 
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MonitorWs {
     fn handle(
         &mut self,
         msg: Result<ws::Message, ws::ProtocolError>,
         ctx: &mut Self::Context,
     ) {
-
-        for i in 0..10 {
-            ctx.binary(format!("testdfasdkfjasfdo{}", i));
-            thread::sleep(time::Duration::from_millis(1000));
-            println!("send messages {}", i);
-        }
-
-        /*
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
             Ok(ws::Message::Text(text)) => ctx.text(text),
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             _ => (),
         }
-        */
     }
 }
 
 
 
-#[get("/ws")]
-async fn wstest(req: HttpRequest, stm: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(MyWs {}, &req, stm);
-    println!("get response: {:?}", resp);
+#[get("/monitor")]
+async fn monitor(req: HttpRequest, stm: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MonitorWs {}, &req, stm);
+    println!("monitor get response: {:?}", resp);
     resp
-}
-
-
-
-#[get("/streaming")]
-async fn streaming() -> HttpResponse {
-    let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
-
-    HttpResponse::Ok()
-        .content_type("application/json")
-        .streaming(body)
 }
 
 
@@ -74,9 +54,8 @@ async fn streaming() -> HttpResponse {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(wstest)
+            .service(monitor)
             .service(fs::Files::new("/", "./static").index_file("index.html"))
-            .service(streaming)
     })
     .bind("0.0.0.0:5000")?
     .run()
